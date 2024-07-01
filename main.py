780890b183fefc38e28f57a964be2b6a04afebf3
Nikita Feldbush <fn11100@gmail.com>
2024-07-01 13:45:58 +0300

    final_by_me

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from git import Repo,Remote,RemoteProgress
-from traceback import format_exception
-repo_url = 'https://github.com/Leteers/auto_git.git'
+import random
+import time
 
-
-def git_push():
+def git_push(commit_message: str):
     try:
         repo = Repo('.')
         repo.git.add('-A')
-        repo.index.commit('init')
+        repo.index.commit(commit_message)
         origin = repo.remote(name='origin')
         origin.push()
     except Exception as e :
         print(f'Some error occured while pushing the code: {e}')    
 
-git_push()
+if __name__ == '__main__':
+    while True:
+        r_number = random.randrange(2,8)
+        for i in range(r_number):
+            with open('text.txt', 'w') as f:
+                f.write(random.randrange(0,10000))
+                f.write(random.randrange(0,10000))
+            git_push(f'{i}/{r_number}')
+            time.sleep((24*60*60)/r_number)
